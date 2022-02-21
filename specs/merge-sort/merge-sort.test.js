5c9c7a135fb8ca6e2370110530a235c6e6f08aab
Scott Schmidt <s@scottschmidt.io>
2022-02-21 13:34:39 -0500

    merge sort; tests pass

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -6,13 +6,39 @@
   You'll need to write more than just one function
 */
 
-const mergeSort = (nums) => {
-  // code goes here
+const merge = (lhsArr, rhsArr) => {
+  let res = [];
+
+  while (lhsArr.length && rhsArr.length) {
+    if (lhsArr[0] <= rhsArr[0]) {
+      res.push(lhsArr.shift());
+    } else {
+      res.push(rhsArr.shift());
+    }
+  }
+
+  return res.concat(lhsArr, rhsArr);
+};
+
+const mergeSort = (arr) => {
+  console.log(`mergeSort([${arr}])`);
+
+  let arrLength = arr.length;
+
+  if (arrLength < 2) {
+    return arr;
+  }
+
+  let breakpoint = Math.floor(arrLength / 2);
+  let lhsArr = arr.slice(0, breakpoint);
+  let rhsArr = arr.slice(breakpoint);
+
+  return merge(mergeSort(lhsArr), mergeSort(rhsArr));
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
